389ca5648e990ea8575f6b5a91aea31889460d1a
ponlponl123 <itsme@ponlponl123.com>
2025-07-23 21:55:14 +0700

    websocket api

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,6 @@
 mod v1 {
     pub mod time;
+    pub mod ws;
 }
 
 use actix_web::web;
@@ -7,6 +8,7 @@ use actix_web::web;
 pub fn config(conf: &mut web::ServiceConfig) {
     conf.service(
         web::scope("/v1")
-            .configure(v1::time::config),
+            .configure(v1::time::config)
+            .route("/ws", web::get().to(v1::ws::echo)),
     );
 }
